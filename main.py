0d20a81dad0058f61c81a6792d0ec8c28586c899
tortopushka <anastasia.komar.24@yandex.ru>
2023-04-27 16:13:54 +0300

    added checking message from user in table Cache

--- main.py
+++ main.py
@@ -13,13 +13,12 @@ env.read_env()
 bot = telebot.TeleBot(env("TOKEN"))
 
 
-def create_cache(text_of_message, language_code, date_of_activity):
-    session_maker = sessionmaker(bind=create_engine(env("URL")))
+def create_cache(session, text_of_message, language_code, date_of_activity, translated_text):
     cache = Cache()
     cache.text_of_message = text_of_message
     cache.language_code = language_code
     cache.date_of_activity = datetime.datetime.utcfromtimestamp(date_of_activity)
-    session = session_maker()
+    cache.russian_translation = translated_text
     session.add(cache)
     session.commit()
 
@@ -32,11 +31,20 @@ def start(message):
 
 @bot.message_handler(func=lambda m: True)
 def translation_into_ru(message):
-    translator = Translator()
-    translated_text = translator.translate(message.text, dest="ru")
-    mess = f'{translated_text.text} \n\n(переведено с {utils.dict_of_lang[translated_text.src]})'
+    session_maker = sessionmaker(bind=create_engine(env("URL")))
+    session = session_maker()
+    check_cache = session.query(Cache.russian_translation, Cache.language_code)\
+        .filter(Cache.text_of_message==message.text).first()
+    if check_cache is None:
+        translator = Translator()
+        translated_text = translator.translate(message.text, dest="ru")
+        mess = f'{translated_text.text} \n\n(переведено с {utils.dict_of_lang[translated_text.src]})'
+        create_cache(session, message.text, translated_text.src, message.date, translated_text.text)
+    else:
+        mess = f'{check_cache.russian_translation} \n\n(переведено с {utils.dict_of_lang[check_cache.language_code]})'
+        print(check_cache.russian_translation)
+    session.close()
     bot.reply_to(message, mess)
-    create_cache(message.text, translated_text.src, message.date)
 
 
 if __name__ == "__main__":
