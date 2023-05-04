d162e90c16b470cb63c5accc06b85c581610eb55
tortopushka <anastasia.komar.24@yandex.ru>
2023-05-04 19:22:53 +0300

    add speech recognition

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from models.models import *
 from db_session import env, session
 import datetime
 import alembic.config
+import os
+import speech_recognition as sr
+import soundfile as sf
 
 
+recognizer = sr.Recognizer()
 bot = telebot.TeleBot(env("TOKEN"))
 
 
@@ -20,6 +24,30 @@ def create_cache(text_of_message: Text, language_code: String, translated_text: 
     session.commit()
 
 
+def translation_into_ru(text_mess):
+    check_cache = session.query(Cache.russian_translation, Cache.language_code) \
+        .filter(Cache.text_of_message == text_mess).first()
+    if check_cache is None:
+        translator = Translator()
+        translated_text = translator.translate(text_mess, dest="ru")
+        mess = f'{translated_text.text} \n\n(переведено с {utils.dict_of_lang[translated_text.src]})'
+        create_cache(text_mess, translated_text.src, translated_text.text)
+    else:
+        mess = f'{check_cache.russian_translation} \n\n(переведено с {utils.dict_of_lang[check_cache.language_code]})'
+    session.close()
+    return mess
+
+
+def recognise(filename):
+    with sr.AudioFile(filename) as source:
+        audio_text = recognizer.listen(source)
+        try:
+            text = recognizer.recognize_google(audio_text, language='en-GB')
+            return text
+        except:
+            return "Sorry.. run again..."
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id,
@@ -27,22 +55,34 @@ def start(message):
 
 
 @bot.message_handler(func=lambda m: True)
-def translation_into_ru(message):
-    check_cache = session.query(Cache.russian_translation, Cache.language_code)\
-        .filter(Cache.text_of_message==message.text).first()
-    if check_cache is None:
-        translator = Translator()
-        translated_text = translator.translate(message.text, dest="ru")
-        mess = f'{translated_text.text} \n\n(переведено с {utils.dict_of_lang[translated_text.src]})'
-        create_cache(message.text, translated_text.src, translated_text.text)
+def text_processing(message):
+    bot.reply_to(message, translation_into_ru(message.text))
+
+
+@bot.message_handler(content_types=['voice'])
+def voice_processing(message):
+    file_info = bot.get_file(message.voice.file_id)
+    filename = str(file_info.file_id)
+    file_name_full = "./voice/"+filename+".ogg"
+    file_name_full_converted = "./ready/"+filename+".wav"
+
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(file_name_full, 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    data, samplerate = sf.read(file_name_full)
+    sf.write(file_name_full_converted, data, samplerate)
+
+    text = recognise(file_name_full_converted)
+    if text == "Sorry.. run again...":
+        bot.reply_to(message, text)
     else:
-        mess = f'{check_cache.russian_translation} \n\n(переведено с {utils.dict_of_lang[check_cache.language_code]})'
-    session.close()
-    bot.reply_to(message, mess)
+        bot.reply_to(message, translation_into_ru(text))
+    os.remove(file_name_full)
+    os.remove(file_name_full_converted)
 
 
 if __name__ == "__main__":
-
     alembicArgs = [
         '--raiseerr',
         'upgrade', 'head',
